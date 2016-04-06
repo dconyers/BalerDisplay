7e75fb302f502f608d817749829a199b1f999c13
Edward Turpin <ed@bjntech.com>
2016-04-06 15:30:06 -0500

    Forgot to add 'this' to variable

--- app/services/Sim800Srvc.ts
+++ app/services/Sim800Srvc.ts
@@ -84,7 +84,7 @@ export function Sim800Srvc() {
     if(this.configState === ConfigState.Invalid) {
       // read config
       exec(
-        "sed -i 's/\\(OK.*AT+CGDCONT.*,.*,\"\\).*\\(\",,0,0\\)/\\1" + apn +"\\2/' /etc/chatscripts/gprs",
+        "sed -i 's/\\(OK.*AT+CGDCONT.*,.*,\"\\).*\\(\",,0,0\\)/\\1" + this.apn +"\\2/' /etc/chatscripts/gprs",
 //        "sed -n 's/OK.*AT+CGDCONT.*,.*,\"\\(.*\\)\",,0,0/\\1/p' ../piSetup/configFiles/gprs",
         this.configRead
       );
@@ -92,7 +92,7 @@ export function Sim800Srvc() {
     else if(this.configState === ConfigState.Dirty) {
       // write config
       exec(
-        "sed -i 's/\\(OK.*AT+CGDCONT.*,.*,\"\\).*\\(\",,0,0\\)/\\1" + apn +"\\2/' /etc/chatscripts/gprs",
+        "sed -i 's/\\(OK.*AT+CGDCONT.*,.*,\"\\).*\\(\",,0,0\\)/\\1" + this.apn +"\\2/' /etc/chatscripts/gprs",
 //      "sed -i 's/\\(OK.*AT+CGDCONT.*,.*,\"\\).*\\(\",,0,0\\)/\\1" + this.apn + "\\2/' ../piSetup/configFiles/gprs",
       this.configWritten
       );
