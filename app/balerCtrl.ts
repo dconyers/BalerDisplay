690f0999c74b034330b19d81135afefcdabba41c
U-SPIDER\dougc <doug.conyers@gmail.com>
2016-04-18 23:14:32 -0500

    Add new jqwidget gauge

--- app/balerCtrl.ts
+++ app/balerCtrl.ts
@@ -6,116 +6,110 @@ import * as q from "q";
 import {BaleTypesDataStore} from "./BaleTypes/BaleTypesDataStore";
 
 export interface BalerData {
-    baleType: BaleType;
-    lowWeight: number;
-    currentWeight: number;
-    highWeight: number;
+  baleType: BaleType;
+  lowWeight: number;
+  currentWeight: number;
+  highWeight: number;
 };
 
 export class BalerCtrl {
 
-    static $inject: string[] = [
-        "$scope",
-        "$log",
-        "LoadCellDataService",
-        "BaleTypesService",
-        "BaleTypesDataStoreService",
-        "BaleEventService"
-    ];
+  static $inject: string[] = [
+    "$scope",
+    "$log",
+    "LoadCellDataService",
+    "BaleTypesService",
+    "BaleTypesDataStoreService",
+    "BaleEventService"
+  ];
 
-    materialList: string[] = [];
+  materialList: string[] = [];
 
-    balerData: BalerData = {
-        baleType: undefined,
-        lowWeight: undefined,
-        currentWeight: undefined,
-        highWeight: undefined,
-    };
+  balerData: BalerData = {
+    baleType: undefined,
+    lowWeight: undefined,
+    currentWeight: undefined,
+    highWeight: undefined,
+  };
 
-    gaugeSettings = {
-        ranges: [{ startValue: 0, endValue: 1000, style: { fill: "#4cb848", stroke: "#4cb848" }, startDistance: 0, endDistance: 0 },
-                 { startValue: 900, endValue: 1200, style: { fill: "#fad00b", stroke: "#fad00b" }, startDistance: 0, endDistance: 0 },
-                 { startValue: 1200, endValue: 1200 * 1.2, style: { fill: "#e53d37", stroke: "#e53d37" }, startDistance: 0, endDistance: 0 }],
-        cap: { size: "5%", style: { fill: "#2e79bb", stroke: "#2e79bb" } },
-        max: 1200 * 1.2,
-        border: { style: { fill: "#8e9495", stroke: "#7b8384", "stroke-width": 1 } },
-        ticksMinor: { interval: 50, size: "5%" },
-        ticksMajor: { interval: 200, size: "10%" },
-        labels: { position: "outside", interval: 200 },
-        pointer: { style: { fill: "#2e79bb" }, width: 5 },
-        animationDuration: 1500,
-        caption: { value: "jQWidgets", position: "bottom", offset: [0, 10], visible: true }
-    };
+  gaugeSettings = {
+    ranges: [{ startValue: 0, endValue: 1000, style: { fill: "#4cb848", stroke: "#4cb848" }, startDistance: 0, endDistance: 0 },
+      { startValue: 900, endValue: 1200, style: { fill: "#fad00b", stroke: "#fad00b" }, startDistance: 0, endDistance: 0 },
+      { startValue: 1200, endValue: 1200 * 1.2, style: { fill: "#e53d37", stroke: "#e53d37" }, startDistance: 0, endDistance: 0 }],
+    cap: { size: "5%", style: { fill: "#2e79bb", stroke: "#2e79bb" } },
+    max: 1200 * 1.2,
+    border: { style: { fill: "#8e9495", stroke: "#7b8384", "stroke-width": 1 } },
+    ticksMinor: { interval: 50, size: "5%" },
+    ticksMajor: { interval: 200, size: "10%" },
+    labels: { position: "outside", interval: 200 },
+    pointer: { style: { fill: "#2e79bb" }, width: 5 },
+    animationDuration: 1500,
+    caption: { value: "Loading, please wait...", position: "bottom", offset: [0, 10], visible: true }
+  };
 
 
-    constructor(private $scope: ng.IScope,
-        private $log: ng.ILogService,
-        private loadCellDataService: LoadCellDataService,
-        private baleTypesService: BaleTypesService,
-        private baleTypesDataStoreService: BaleTypesDataStore,
-        private baleEventService: BaleEventService) {
-        $log.debug("Top of BalerCtrl constructor");
-        this.refreshData();
-    }
+  constructor(private $scope: ng.IScope,
+    private $log: ng.ILogService,
+    private loadCellDataService: LoadCellDataService,
+    private baleTypesService: BaleTypesService,
+    private baleTypesDataStoreService: BaleTypesDataStore,
+    private baleEventService: BaleEventService) {
+    this.refreshData();
+  }
 
-    public currentBaleTypeChangeRequest(newCurrent: BaleType): q.Promise<any> {
-        return q.fcall(() => {
-            if (this.balerData.baleType !== null) {
-                this.$log.debug("it's not undefined: " + this.balerData.baleType);
-                return this.baleTypesDataStoreService.updateRowPromise(this.balerData.baleType._id, { $set: { currentType: false } }, {});
-            }
-        }).then(() => {
-            return this.baleTypesDataStoreService.updateRowPromise(newCurrent._id, { $set: { currentType: true } }, {});
-        }).catch((exception: any) => {
-                this.$log.error("balerCtrl::currentBaleTypeChangeRequest Got exception" + exception);
-                return exception;
-        });
-    }
+  public currentBaleTypeChangeRequest(newCurrent: BaleType): q.Promise<any> {
+    return q.fcall(() => {
+      if (this.balerData.baleType !== null) {
+        return this.baleTypesDataStoreService.updateRowPromise(this.balerData.baleType._id, { $set: { currentType: false } }, {});
+      }
+    }).then(() => {
+      return this.baleTypesDataStoreService.updateRowPromise(newCurrent._id, { $set: { currentType: true } }, {});
+    }).catch((exception: any) => {
+      this.$log.error("balerCtrl::currentBaleTypeChangeRequest Got exception" + exception);
+      return exception;
+    });
+  }
 
-    public dougTest(): void {
-        this.$log.debug("top of dougTest()");
-        this.refreshData();
-    }
+  public dougTest(): void {
+    this.$log.debug("top of dougTest() lowWeight: " + this.balerData.lowWeight + ". High: " + this.balerData.highWeight);
+  }
 
-    public loadBaleTypeData(): q.Promise<any> {
-        this.$log.debug("Top of loadBaleTypeData");
-        return  this.baleTypesService.getMaterialList().then((retVal: string[]) => {
-                    this.materialList = retVal;
-                });
-    }
+  public loadBaleTypeData(): q.Promise<any> {
+    return this.baleTypesService.getMaterialList().then((retVal: string[]) => {
+      this.materialList = retVal;
+    });
+  }
 
-    private refreshData() {
-        this.$log.debug("Top of refreshData()");
+  private refreshData() {
+    this.baleTypesDataStoreService.initializeDataStore()
+      .then(() => {
+        return this.baleTypesService.getCurrentBaleType();
+      }).then((returnVal: BaleType) => {
+        this.balerData.baleType = returnVal;
+        this.balerData.lowWeight = returnVal.min;
+        this.balerData.highWeight = returnVal.max;
 
-        this.baleTypesDataStoreService.initializeDataStore()
-        .then(() => {
-            return this.baleTypesService.getCurrentBaleType();
-        }).then ((returnVal: BaleType) => {
-            this.$log.debug("balerCtrl::refreshData");
-            this.balerData.baleType = returnVal;
-            this.balerData.lowWeight = returnVal.min;
-            this.balerData.highWeight = returnVal.max;
+        this.gaugeSettings.max = this.balerData.highWeight * 1.2;
+        this.gaugeSettings.ranges[0].endValue = this.balerData.lowWeight;
+        this.gaugeSettings.ranges[1].startValue = this.balerData.lowWeight;
+        this.gaugeSettings.ranges[1].endValue = this.balerData.highWeight;
+        this.gaugeSettings.ranges[2].startValue = this.balerData.highWeight;
+        this.gaugeSettings.ranges[2].endValue = this.balerData.highWeight * 1.2;
 
-            this.gaugeSettings.max = this.balerData.highWeight * 1.2;
-            this.gaugeSettings.ranges[0].endValue = this.balerData.lowWeight;
-            this.gaugeSettings.ranges[1].startValue = this.balerData.lowWeight;
-            this.gaugeSettings.ranges[1].endValue = this.balerData.highWeight;
-            this.gaugeSettings.ranges[2].startValue = this.balerData.highWeight;
-            this.gaugeSettings.ranges[2].endValue = this.balerData.highWeight * 1.2;
-            this.gaugeSettings.apply();
-            this.$scope.$apply();
-            this.gaugeSettings.refresh();
-        }).done;
+        this.gaugeSettings.apply("ranges", this.gaugeSettings.ranges);
+        this.gaugeSettings.apply("max", this.gaugeSettings.max);
 
+      }).done;
+
+    this.$scope.$on("jqxGaugeCreated", (event) => {
+      this.$scope.$watch(() => {
+        return this.loadCellDataService.getLoadCellWeight();
+      }, () => {
         this.balerData.currentWeight = this.loadCellDataService.getLoadCellWeight();
-        this.$scope.$watch(() => {
-            return this.loadCellDataService.getLoadCellWeight();
-        }, () => {
-            // this.balerData.currentWeight = this.loadCellDataService.getLoadCellWeight();
-            // this.gaugeSettings.caption.value = " " + this.balerData.currentWeight;
-            // this.$log.debug("currentWeight updated to: " + this.gaugeSettings.caption.value);
-
-        });
-    }
+        this.gaugeSettings.caption.value = "Current Weight: " + this.balerData.currentWeight;
+        this.gaugeSettings.apply("caption", this.gaugeSettings.caption);
+      });
+    });
+  }
 
 };
