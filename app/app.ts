1ba2162acb9047725267509ef1f31f724bc44260
dconyers <doug.conyers@gmail.com>
2016-05-03 16:54:36 -0500

    Added log4js Support

--- app/app.ts
+++ app/app.ts
@@ -34,6 +34,12 @@ angular.module("balerApp", [
                              "jqwidgets"
                            ]
 )
+  .factory("$log", function() {
+    let log4js = require("log4js");
+    log4js.configure("log4js.json", { reloadSecs: 10 });
+    let logger = log4js.getLogger();
+    return logger;
+  })
     .service("BaleTypesDataStoreService", BaleTypesDataStore)
     .service("BaleTypesService", BaleTypesService)
     .service("PictureSrvc", PictureSrvc)
